12c708acc8361ad06d4cccd77f13cae8f788455b
Anwar Barakat <brkatanwar0@gmail.com>
2022-06-25 17:59:49 +0300

    output of screen and console methods

--- js/0_introduction.js
+++ js/0_introduction.js
@@ -4,3 +4,26 @@
   2-  working on client and server side
   3-  implement complex features on a web page by making it interactive
 */
+
+
+
+/*
+  output to screen : 
+  1-  window.alert('Hi')
+  2-  console.log('Hi')
+  3-  document.write('Hi')
+*/
+
+
+
+/*
+  console methods :
+  1-  log
+  2-  error
+  3-  table
+
+  styling console : 
+  - Directive %c
+*/
+
+console.log('Hello from %cJS %cfile', "color:red;font-size:40px", "color:blue;font-size:20px");
