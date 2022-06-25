945296840325daf63a16caafaa3514a5312034a3
Anwar Barakat <brkatanwar0@gmail.com>
2022-06-25 16:47:31 +0300

    initiate  javascript bootcamp course

---
+++ js/0_introduction.js
@@ -0,0 +1,6 @@
+/*
+  what is javascript : 
+  1-  scripting or programming language.
+  2-  working on client and server side
+  3-  implement complex features on a web page by making it interactive
+*/
