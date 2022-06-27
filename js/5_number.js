b27267c06e1e697863169e502eeae7dcaf690948
Anwar Barakat <brkatanwar0@gmail.com>
2022-06-27 15:36:48 +0300

    number methods

---
+++ js/5_number.js
@@ -0,0 +1,20 @@
+/*
+  Number : 
+  1-  syntactic suger "_"
+  2-  e
+  3-  **  
+  4-  with decimal
+  5-  number + bigInt
+  6-  number + min value
+  7-  number + max value
+*/
+
+
+console.log(1000000) // 1000000
+    // console.log(1 _000_000) // 1000000
+console.log(1e6) // 10000000
+console.log(10 ** 6) // 1000000
+console.log(1000000.0000) // 1000000
+console.log(1000000.1) // 1000000.1
+console.log(Number.MAX_SAFE_INTEGER) // 9007199254740991
+console.log(Number.MAX_VALUE) // 1.7976931348623157e+308
