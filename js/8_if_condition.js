0f51561c3eaae7ad3287ca14e3eed8abaca8e592
Anwar Barakat <brkatanwar0@gmail.com>
2022-06-28 14:14:56 +0300

    conditional ternary operator

--- js/8_if_condition.js
+++ js/8_if_condition.js
@@ -17,3 +17,30 @@ else if (country == 'Egypt') {
     price -= discountAmount - 10;
 
 console.log(price); // 40
+
+
+
+/*
+  conditonal ternary operator : 
+  condition ? true : false ;
+*/
+
+let theName = "Anwar",
+    theGender = "Male",
+    theAge = 25;
+
+
+if (theGender == "Male") {
+    console.log('Mr');
+} else {
+    console.log('Mrs');
+}
+
+let theValue = theGender == "Male" ? 'Mr' : 'Mrs';
+console.log(theValue);
+console.log(`Hello ${theValue}. ${theName}`); // Hello Mr. Anwar
+
+
+let theValue2 = theAge < 25 ? '25' : theAge >= 25 && theAge <= 50 ? '25 - 50' : theAge > 50 ? 'larger than 50' : 'undefined';
+
+console.log(theValue2) // 25 - 50
