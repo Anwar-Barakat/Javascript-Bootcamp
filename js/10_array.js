9ce2077ba79a6a4136b0827a28240bc28fd1b807
Anwar Barakat <brkatanwar0@gmail.com>
2022-06-29 15:30:00 +0300

    arrays methods part (2) array length , adding and removing elements

--- js/10_array.js
+++ js/10_array.js
@@ -22,3 +22,51 @@ console.log(myFriend)
 
 console.log(typeof myFriend) // object
 console.log(Array.isArray(myFriend)) // true
+
+
+/*
+  array length
+*/
+
+console.log(myFriend.length); // 4
+
+// myFriend[6] = "Huda";
+// console.log(myFriend.length); // 7 => now, there are two empty element in this array
+// to add the element in the end of that array dynamically : 
+
+myFriend[myFriend.length] = "Huda";
+console.log(myFriend.length); // 5
+
+// for example i wanna update the last element in that array :
+
+console.log(myFriend); // ['Anwar', 'Mohamed', 'Ali', Array(2), 'Huda']
+console.log(myFriend.length); // 5
+myFriend[myFriend.length - 1] = "Zide";
+console.log(myFriend); //  ['Anwar', 'Mohamed', 'Ali', Array(2), 'Zide']
+console.log(myFriend.length); // 5
+
+
+
+/*
+  Array methods : (adding and removing)
+  1-  unshift(" ", " ") add the elements at the first 
+  2-  push(" ", " ") add the elements at the end
+  3-  shift() remove from the first 
+  4-  
+*/
+
+let theFriend = ["Anwar", "Mohamed", "Ali", "Gamel", "Housam"];
+console.log(theFriend); // (5) ['Anwar', 'Mohamed', 'Ali', 'Gamel', 'Housam']
+
+theFriend.unshift("Omar", "Samer");
+console.log(theFriend); // (7) ['Omar', 'Samer', 'Anwar', 'Mohamed', 'Ali', 'Gamel', 'Housam']
+
+theFriend.push("Said", "John");
+console.log(theFriend); // (9) ['Omar', 'Samer', 'Anwar', 'Mohamed', 'Ali', 'Gamel', 'Housam', 'Said', 'John']
+
+theFriend.shift();
+console.log(theFriend); // (8) ['Samer', 'Anwar', 'Mohamed', 'Ali', 'Gamel', 'Housam', 'Said', 'John']
+
+let theLast = theFriend.pop();
+console.log(theFriend); // (7) ['Samer', 'Anwar', 'Mohamed', 'Ali', 'Gamel', 'Housam', 'Said']
+console.log(theLast); // John
