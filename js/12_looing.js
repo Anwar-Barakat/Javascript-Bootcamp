02c04b20f4348d85979471cebcf1285ed31e9210
Anwar Barakat <brkatanwar0@gmail.com>
2022-06-30 18:24:13 +0300

    nested loop

--- js/12_looing.js
+++ js/12_looing.js
@@ -17,3 +17,33 @@ for (let j = 0; j < myFriend.length; j++) {
 }
 
 console.log(justNames)
+
+
+
+
+/*
+  nested looping 
+*/
+
+let products = ['mouse', 'keyboard', 'screen', 'pen', 'pad', 'tablet'];
+let colors = ['red', 'blue', 'green', 'black'];
+let models = [2021, 2022];
+
+for (let k = 0; k < products.length; k++) {
+    console.log('-'.repeat(15));
+    console.log('# ' + products[k])
+    console.log('-'.repeat(15));
+
+    console.log('Colors : \n')
+    for (let l = 0; l < colors.length; l++) {
+        console.log(colors[l])
+    }
+
+    console.log('Models : \n')
+    for (let m = 0; m < models.length; m++) {
+        console.log(models[m])
+
+    }
+
+
+}
