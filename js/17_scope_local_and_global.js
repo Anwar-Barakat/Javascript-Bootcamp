1accf28fd0ccd65901497f61f3a129b9bd2f7548
Anwar Barakat <brkatanwar0@gmail.com>
2022-07-02 14:02:18 +0300

    lexical scope with examples

--- js/17_scope_local_and_global.js
+++ js/17_scope_local_and_global.js
@@ -26,3 +26,61 @@ function showLocaly() {
     console.log(`Function - from global ${b}`); // 20
 }
 showLocaly();
+
+
+/*
+  Scope : 
+  - Lexical Scope
+*/
+
+// example 1 : 
+function parent_1() {
+    let a = 10;
+
+    function child() {
+        console.log(a); // 10
+    }
+    child()
+}
+parent_1();
+
+// example 2 : 
+function parent_2() {
+    let a = 10;
+
+    function child() {
+        let a = 20;
+        console.log(a); // 20
+    }
+    child()
+}
+parent_2();
+
+// example 3 : 
+let aa = 10;
+
+function parent_3() {
+
+    function child() {
+        console.log(aa); // 10
+    }
+    child()
+}
+parent_3();
+
+// example 3 : 
+function parent_3() {
+
+    function child() {
+        console.log(a); // 10
+
+        console.log(`From grand : ${bb}`); // error => bb is not defined
+
+        function grand() {
+            let bb = 30;
+            console.log(`From grand : ${bb}`); // 30
+        }
+    }
+    child()
+}
+parent_3();
