6301b9361a1543ae1a3b2fe7a9174b2ca07bc2b6
Anwar Barakat <brkatanwar0@gmail.com>
2022-07-04 14:43:27 +0300

    events, classlist methods

--- js/24_events.js
+++ js/24_events.js
@@ -27,8 +27,31 @@ document.forms[0].onsubmit = function(e) {
     }
 }
 
-let googleLink = document.querySelector('.google');
+/*
+    Events 
+*/
 
-googleLink.onclick = function(event) {
-    event.preventDefault();
+// dealing with classes element : 
+
+let myElement = document.querySelector('.my-div');
+
+// get the classes as a collection : 
+let classes = myElement.classList;
+
+// check if specific class is exists or not :
+let exits = myElement.classList.contains('active'); // true or false
+
+// get the class using its index :
+let firstClass = myElement.classList.item("0") // first class from group of classes
+let secondClass = myElement.classList.item("1")
+
+myElement.onclick = function() {
+    this.classList.remove('active ')
+
+    this.classList.add('first', 'second');
+}
+
+// each click will toggle this class, if it is exists => will remove it or will add it. 
+myElement.onclick = function() {
+    this.classList.toggle('active')
 }
