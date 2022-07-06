2e0caf9acfe0c574af219a1638e539f77c5c9a3d
Anwar Barakat <brkatanwar0@gmail.com>
2022-07-06 12:01:18 +0300

    BOM, local storage vs session storage

--- js/28_local storage.js
+++ js/28_local storage.js
@@ -29,3 +29,31 @@ window.localStorage.removeItem('fontWeight')
 
 // remove the whole storage
 window.localStorage.clear();
+
+
+/*
+  BOM
+  Session Storage Vs Local Storage:
+  When I close the chrome tab or close the browser , the sessions will remove
+  In comparison with, Items in storage will stay
+
+  new tap = new session
+  duplicate tab = copy session
+*/
+
+// local storage and session storage separated from each other
+window.localStorage.setItem('color','red');
+
+window.sessionStorage.setItem('color','red')
+
+let usernameInput = document.querySelector("[name=username]");
+usernameInput.onblur = ()=>{
+
+  window.sessionStorage.setItem('username-input', this.value);
+
+};
+
+if(window.sessionStorage.getItem('username-input')){
+
+  usernameInput.value = window.sessionStorage.getItem('username-input')
+}
