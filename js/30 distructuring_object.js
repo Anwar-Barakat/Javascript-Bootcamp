044f06603b0d54585b1c4e74367403b2c1817ed7
Anwar Barakat <brkatanwar0@gmail.com>
2022-07-07 11:33:41 +0300

    destructuring function parameter

--- js/30 distructuring_object.js
+++ js/30 distructuring_object.js
@@ -27,3 +27,43 @@ let { x, y, z } = user;
 console.log(x); // undefined  => we must use the same object key
 console.log(y); // undefined  => we must use the same object key
 console.log(z); // undefined  => we must use the same object key
+
+/*
+  Destructuring Function Parameter
+*/
+
+const username = {
+  theName: "Anwar",
+  theAge: 25,
+  skills: {
+    html: 70,
+    css: 60,
+  },
+};
+
+showDetails(username);
+
+function showDetails(object) {
+  console.log(`Your name is ${object.theName}`); // Your name is Anwar
+  console.log(`Your age is ${object.theAge}`); // Your age  is Anwar
+  console.log(`Your css skill progress is ${object.skills.css}`); // Your css skill progress is 60
+}
+
+// use destructuring function parameter
+
+showInfo();
+
+function showInfo({ theName, theAge, skills: { css } } = username) {
+  console.log(`Your name is ${theName}`); // Your name is Anwar
+  console.log(`Your age is ${theAge}`); // Your age  is Anwar
+  console.log(`Your css skill progress is ${css}`); // Your css skill progress is 60
+}
+
+// use destructuring function parameter with alias
+showInfoormation();
+
+function showInfoormation({ theName : n, theAge : a, skills: { css : c } } = username) {
+  console.log(`Your name is ${n}`); // Your name is Anwar
+  console.log(`Your age is ${a}`); // Your age  is Anwar
+  console.log(`Your css skill progress is ${c}`); // Your css skill progress is 60
+}
