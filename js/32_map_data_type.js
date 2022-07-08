02fb46e9a99637886977dd324ea6ea73ff5a05df
Anwar Barakat <brkatanwar0@gmail.com>
2022-07-08 13:58:34 +0300

    map data type

---
+++ js/32_map_data_type.js
@@ -0,0 +1,28 @@
+/*
+  Map Data Type :
+*/
+
+let myNewObject = {
+  10: "number",
+  10: "string",
+};
+console.log(myNewObject[10]); // string => because overrideing
+console.log(myNewObject["10"]); // string => because overrideing
+
+let myNewMap = new Map();
+
+// addition :
+myNewMap.set(10, "number");
+myNewMap.set("10", "string");
+
+console.log(myNewMap.get(10)); // number =>
+console.log(myNewMap.get("10")); // string =>
+
+console.log(myNewObject); // {10: 'string'}
+console.log(myNewMap); // Map(2) {10 => 'number', '10' => 'string'}
+
+console.log(myNewMap.size); // 2
+
+myNewMap.set(true, "boolean");
+myNewMap.set({ address_1: "damas", address_2: "yabroud" }, "object");
+myNewMap.set(function sayHello() {}, "function");
