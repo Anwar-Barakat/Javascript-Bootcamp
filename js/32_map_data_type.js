72efc71e5c76ef457fa1d1084f375e6fb0b79934
Anwar Barakat <brkatanwar0@gmail.com>
2022-07-09 21:41:20 +0300

    Map vs WeakMap

--- js/32_map_data_type.js
+++ js/32_map_data_type.js
@@ -27,3 +27,25 @@ myNewMap.set(true, "boolean");
 myNewMap.set({ address_1: "damas", address_2: "yabroud" }, "object");
 myNewMap.set(function sayHello() {}, "function");
 
+/*
+  Map vs WeakMap : 
+
+  Map       => key can be anything 
+  WeakMap   => key can Be exclusively object
+*/
+
+let mapUser = { theName: "Anwar" };
+
+let myMap = new Map();
+
+myMap.set(mapUser, "Object Value");
+
+console.log(myMap); // Map(1) {{…} => 'Object Value'}
+
+let weakMapUser = { theName: "Anwar" };
+
+let myWeakMap = new WeakMap();
+
+myWeakMap.set(weakMapUser, "Object value");
+
+myWeakMap.set("Name", "Object value"); //  Invalid value used as weak map key
