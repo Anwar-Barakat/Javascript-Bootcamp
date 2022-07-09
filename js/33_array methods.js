42099bd2a5b2a0aa82348abfcf4e800f370a514e
Anwar Barakat <brkatanwar0@gmail.com>
2022-07-09 22:57:15 +0300

    array method , Array.copyWithin

--- js/33_array methods.js
+++ js/33_array methods.js
@@ -32,3 +32,45 @@ console.log(Array.from(mySet)); // (4) [1, 2, 3, 4]
 console.log([new Set(myArray)]); // 0: Set(4) {1, 2, 3, 4}
 
 console.log([...new Set(myArray)]); // (4) [1, 2, 3, 4]
+
+/*
+  Array method 
+  Array.copyWithin(target, start [opt], end [opt])
+  copy part of an array to another location in the same array
+  target : 
+    index to copy part to
+
+  start : 
+    index to start copying with
+  
+  end 
+    index to end copying with
+*/
+
+let myCopyArray = [10, 20, 30, 40, 50, "A", "B"];
+
+// myCopyArray.copyWithin(3);
+
+/*
+  start from 40
+   there isn't start => so start from 0
+   10, 20, 30, 40, 50, "A", "B"
+   we but it instead of 40
+   10, 20, 30, 10, 20, 30, 40, 50, "A", "B", 50, "A", "B"
+   the number of array is 7 
+   start from 0 => [10, 20, 30, 10, 20, 30, 40]
+*/
+
+// console.log(myCopyArray); // (7) [10, 20, 30, 10, 20, 30, 40]
+
+// myCopyArray.copyWithin(4, 6);
+
+// console.log(myCopyArray); // (7) [10, 20, 30, 40, 'B', 'A', 'B']
+
+// myCopyArray.copyWithin(1, -2);
+
+// console.log(myCopyArray); // (7) [10, 'A', 'B', 40, 50, 'A', 'B']
+
+myCopyArray.copyWithin(1, -2, -1);
+
+console.log(myCopyArray); // (7) (7) [10, 'A', 30, 40, 50, 'A', 'B']
