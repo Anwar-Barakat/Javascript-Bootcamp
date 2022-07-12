02470639de1fb413984b0e0e35c7b76613c2fb45
Anwar Barakat <brkatanwar0@gmail.com>
2022-07-12 23:20:19 +0300

    OOP, dealing with props and methods

--- js/36 oop.js
+++ js/36 oop.js
@@ -2,32 +2,34 @@
   OOP : 
 
   Construction Function
+    - Dealing with properties and method
 */
-
-//? old way to create constuctor :
-// function User(id, name, salary) {
-//   this.user_id = id;
-//   this.username = name;
-//   this.user_salary = salary + 1000;
-// }
 
 //? new way to create constuctor :
 class User {
   constructor(id, name, salary) {
+    // props :
     this.user_id = id;
-    this.username = name;
-    this.user_salary = salary + 1000;
+    this.username = name || "unknown";
+    this.user_salary = salary < 6000 ? salary + 500 : salary;
+    this.msg = () => {
+      return `Hi ${this.username}, your salary is ${this.salary}`;
+    };
+  }
+
+  // methods :
+  sayHello() {
+    return `Hi ${this.username}, your salary is ${this.salary}`;
   }
 }
 
-let firstUser = new User("1", "Anwar", 5000);
+let firstUser = new User("1", "", 5000);
 let secondUser = new User("2", "Belal", 6000);
 let thirdUser = new User("3", "Ali", 7000);
 
 console.log(firstUser.user_id); // 1
-console.log(firstUser.username); // Anwar
-console.log(firstUser.user_salary); // 6000
+console.log(firstUser.username); // unknown
+console.log(firstUser.user_salary); // 5500
+console.log(firstUser.msg()); // Hi unknown, your salary is undefined
 
-console.log(firstUser instanceof User); // true
-
-console.log(firstUser.constructor === User); // true
+console.log(firstUser.sayHello()); // Hi unknown, your salary is undefined
