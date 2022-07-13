87d0c437d5a223e7667bd74a174e23f356a5100b
Anwar Barakat <brkatanwar0@gmail.com>
2022-07-13 15:08:29 +0300

    OOP, object meta data and descriptor

--- js/User.js
+++ js/User.js
@@ -34,3 +34,60 @@ String.prototype.addDotBeforeAndAfter = function () {
 let myString = "Anwar Barakat";
 
 console.log(myString.addDotBeforeAndAfter()); // .Anwar Barakat.
+
+/*
+  Object Meta Data And Descriptor
+
+  the default values of it are false 
+*/
+
+const myOb = {
+  a: 1,
+  b: 2,
+};
+
+Object.defineProperty(myOb, "c", {
+  writable: false,
+  enumerable: false,
+  configurable: false,
+  value: 3,
+});
+
+console.log(myOb.c); // 3
+
+// change the "c" value
+myOb.c = 100;
+
+console.log(myOb.c); // 100
+
+// if writable : false we can't change it
+myOb.c = 100;
+
+console.log(myOb.c); // 3
+
+// looping :
+for (const prop in myOb) {
+  console.log(`${prop} , ${myOb[prop]}`);
+}
+
+// if enumerable : false , c will go out the looping
+
+// we can also delete object property :
+console.log(delete myOb.c); // true
+
+// if configurable : false we can't delete this prop or redefine "c" property
+
+Object.defineProperties(myOb, {
+  d: {
+    writable: true,
+    value: 5,
+  },
+  e: {
+    enumerable: true,
+    value: 6,
+  },
+  f: {
+    configurable: false,
+    value: 7,
+  },
+});
