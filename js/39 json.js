b33e911c149efc568de4d1bfe649298ff6a7eb32
Anwar Barakat <brkatanwar0@gmail.com>
2022-07-15 23:09:32 +0300

    synchronous vs asynchronous , call stack javascript engine

--- js/39 json.js
+++ js/39 json.js
@@ -46,3 +46,73 @@ let sendDateToServer = JSON.stringify(myJsObject);
 console.log(typeof sendDateToServer); // string
 
 console.log(sendDateToServer); // {"username":"Anwar Barakat","age":25}
+
+/*
+  Synchronous & Asynchronous : 
+
+  Synchronous : 
+  -	Operations runs sequentially.
+  -	Each operation must wait the previous one to finish
+
+  Asynchronous:
+  -	Operations runs parallelly
+  -	An operation can occur while another one is still run.
+*/
+
+/*
+  Call Stack || Stack Trace: 
+  -	Javascript Engine uses a Call Stack to manage excution contexts.
+  -	Mechanism to make the Interpreter track your calls.
+  -	When function called a function, will add it in the stack.
+  -	When function excuted a function will removed it from the stack.
+  -	Work using LIFO Principle => Last in First Out.
+  - Call Stack detect web api methods and leave it to the browser to handle it
+*/
+
+setTimeout(() => {
+  console.log("web api");
+}, 0);
+
+function one() {
+  console.log("one");
+}
+
+function two() {
+  one();
+  console.log("two");
+}
+function three() {
+  two();
+  console.log("three");
+}
+
+three();
+
+/* Stack  => LIFO
+  =====================================
+  =====================================
+  =====================================
+  =====================================
+  function one() {
+    console.log("one");
+  }
+  =====================================
+  function two() {
+    one();
+    console.log("two");
+  }
+  =====================================
+  function three() {
+    two();
+    console.log("three");
+  }
+  =====================================
+*/
+
+// three() excution
+console.log("===============");
+console.log("one");
+console.log("two");
+console.log("three");
+
+// the output will be :  one tow three web api
