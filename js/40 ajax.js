58b061ae9afef94f71421f5b4fbe66141abe593d
Anwar Barakat <brkatanwar0@gmail.com>
2022-07-19 14:58:12 +0300

    into to ajax

---
+++ js/40 ajax.js
@@ -0,0 +1,39 @@
+/*
+  AJAX :
+
+  -	Asynchronous Javascript And XML.
+  -	An Approach to use several technologies together [ HTML – CSS – JS ].
+  -	It use “ XMLHttpRequest ”object to interact with the sever.
+  -	You can fetch and send data without page refresh.
+
+
+  Ready State   	=> status of the request
+  0	  => request not initialized
+  1	  => server connection established
+  2	  => request received
+  3	  => Processing request
+  4	  => request is finished and response is ready
+
+  Status : 
+  200 => response is successful
+  404 => not found
+
+*/
+
+let myRequest = new XMLHttpRequest();
+
+myRequest.open("GET", "https://api.github.com/users/Anwar-Barakat/repos", true);
+
+myRequest.send();
+
+console.log(myRequest);
+
+myRequest.onreadystatechange = () => {
+  console.log(myRequest.readyState);
+
+  console.log(myRequest.status);
+
+  if (myRequest.readyState === 4 && myRequest.status === 200) {
+    console.log(myRequest.responseText);
+  }
+};
